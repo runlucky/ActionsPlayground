eb9712edc67db037826380400f6fe29974baae25
Kakeru Fukuda <stand.by.we@gmail.com>
2020-01-04 18:47:37 +0900

    多言語ファイル追加

---
+++ ActionsPlayground/Localizable/Localizable.swift
@@ -0,0 +1,36 @@
+// ### 注意 ###
+// このファイルは直接編集しないでください。
+// 文言を追加・修正したい場合はLocalizable.csvを編集してから
+// Localizer.appを実行してください。
+
+internal enum Localizable {
+    /// ホーム / Home 
+    case home
+    /// ダッシュボード / Dashboard 
+    case Dash_board
+    /// 現在(%@) / Today(%@) 
+    case Today(String)
+    /// %@から%@に変更します。\nよろしいですか？ / Change from %@ to %@. Will you? 
+    case change(String, String)
+    /// 処理中... (%d/%d) / Processing... (%d/%d) 
+    case Progress(Int, Int)
+    /// インフォメーション / Information 
+    case Information
+    /// 一般 / General 
+    case General
+    /// ローカライズ済みの文言を指定してください。 
+    case localized(String)
+
+    internal var localize: String {
+        switch self {
+        case .home: return "home".localize
+        case .Dash_board: return "Dash board".localize
+        case .Today(let x1): return "Today".localize(arguments: [x1])
+        case .change(let x1, let x2): return "change".localize(arguments: [x1, x2])
+        case .Progress(let x1, let x2): return "Progress".localize(arguments: [x1, x2])
+        case .Information: return "Information".localize
+        case .General: return "General".localize
+        case .localized(let x1): return x1
+        }
+    }
+}
